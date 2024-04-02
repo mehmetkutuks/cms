43e68ee178d15188f59059416e5bfea980593f87
Mehmet Kutuk <mkutuk42550@gmail.com>
2024-04-02 10:27:55 +0300

    tema ayarları yapıldı

--- panel/assets/assets/js/app.js
+++ panel/assets/assets/js/app.js
@@ -554,6 +554,18 @@
 // initialize app
 +function($, window) { 'use strict';
 	window.app.init();
+
+	window.app.menubar.setTheme("dark");
+	window.app.menubar.applyTheme();
+	window.app.saveSettings();
+
+
+	window.app.navbar.setTheme("dark");
+	window.app.navbar.applyTheme();
+	window.app.saveSettings();
+
+
+	
 	window.app.menubar.init();
 	window.app.navbar.init();
 	window.app.customizer.init();
