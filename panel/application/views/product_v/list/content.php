bef5cbcbb1281ad96e6d2ed98c1a74f3a4dd3ff3
Mehmet Kutuk <mkutuk42550@gmail.com>
2024-04-04 01:58:11 +0300

    toggle buton eklendi, kayıt yoktur alert eklendi

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,9 +1,17 @@
 <div class="row">
     <div class="col-md-12">
-		<h4 class="m-b-lg">Product List</h4>
+		<h4 class="m-b-lg">
+            Product List
+            <a href="" class="btn btn-outline btn-primary btn-xs pull-right"><i class="fa fa-plus"> New Product</i></a>
+        </h4>
 	</div><!-- END column -->
     <div class="col-md-12">
 		<div class="widget p-lg">
+
+        <div class="alert alert-danger text-center">
+			<p>There is no data here. Please <a href="">click here</a>.</p>
+		</div>
+
 			<table class="table table-hover table-striped">
                 <thead>
                     <th>#id</th>
@@ -20,7 +28,22 @@
                         <td>monitor-askisi</td>
                         <td>Monitör Askısı</td>
                         <td>360 derece kullanılan monitör askısı</td>
-                        <td>1/0</td>
+                        <td>
+							<input id="switch-2-2" data-size="small" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
+                        <td>
+                            <a href="" class="btn btn-xs btn-danger btn-outline" title="delete"><i class="fa fa-trash"></i> Delete</a>
+                            <a href="" class="btn btn-xs btn-info btn-outline" title="edit"><i class="fa fa-pencil-square-o"></i> Edit</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 derece kullanılan monitör askısı</td>
+                        <td>
+							<input id="switch-2-2" data-size="small" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
                         <td>
                             <a href="" class="btn btn-xs btn-danger btn-outline" title="delete"><i class="fa fa-trash"></i> Delete</a>
                             <a href="" class="btn btn-xs btn-info btn-outline" title="edit"><i class="fa fa-pencil-square-o"></i> Edit</a>
@@ -32,7 +55,9 @@
                         <td>monitor-askisi</td>
                         <td>Monitör Askısı</td>
                         <td>360 derece kullanılan monitör askısı</td>
-                        <td>1/0</td>
+                        <td>
+							<input id="switch-2-2" data-size="small" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
                         <td>
                             <a href="" class="btn btn-xs btn-danger btn-outline" title="delete"><i class="fa fa-trash"></i> Delete</a>
                             <a href="" class="btn btn-xs btn-info btn-outline" title="edit"><i class="fa fa-pencil-square-o"></i> Edit</a>
@@ -44,7 +69,51 @@
                         <td>monitor-askisi</td>
                         <td>Monitör Askısı</td>
                         <td>360 derece kullanılan monitör askısı</td>
-                        <td>1/0</td>
+                        <td>
+							<input id="switch-2-2" data-size="small" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
+                        <td>
+                            <a href="" class="btn btn-xs btn-danger btn-outline" title="delete"><i class="fa fa-trash"></i> Delete</a>
+                            <a href="" class="btn btn-xs btn-info btn-outline" title="edit"><i class="fa fa-pencil-square-o"></i> Edit</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 derece kullanılan monitör askısı</td>
+                        <td>
+							<input id="switch-2-2" data-size="small" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
+                        <td>
+                            <a href="" class="btn btn-xs btn-danger btn-outline" title="delete"><i class="fa fa-trash"></i> Delete</a>
+                            <a href="" class="btn btn-xs btn-info btn-outline" title="edit"><i class="fa fa-pencil-square-o"></i> Edit</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 derece kullanılan monitör askısı</td>
+                        <td>
+							<input id="switch-2-2" data-size="small" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
+                        <td>
+                            <a href="" class="btn btn-xs btn-danger btn-outline" title="delete"><i class="fa fa-trash"></i> Delete</a>
+                            <a href="" class="btn btn-xs btn-info btn-outline" title="edit"><i class="fa fa-pencil-square-o"></i> Edit</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 derece kullanılan monitör askısı</td>
+                        <td>
+							<input id="switch-2-2" data-size="small" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
                         <td>
                             <a href="" class="btn btn-xs btn-danger btn-outline" title="delete"><i class="fa fa-trash"></i> Delete</a>
                             <a href="" class="btn btn-xs btn-info btn-outline" title="edit"><i class="fa fa-pencil-square-o"></i> Edit</a>
