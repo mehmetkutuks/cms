9ec617d43932f89bbd2963eb620fc8f06374b31e
Mehmet Kutuk <mkutuk42550@gmail.com>
2024-04-03 14:36:08 +0300

    urunler modulunun tablo yapisi olusturuldu

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,58 @@
-ürün listesi content
+<div class="row">
+    <div class="col-md-12">
+		<h4 class="m-b-lg">Product List</h4>
+	</div><!-- END column -->
+    <div class="col-md-12">
+		<div class="widget p-lg">
+			<table class="table table-hover table-striped">
+                <thead>
+                    <th>#id</th>
+                    <th>url</th>
+                    <th>title</th>
+                    <th>description</th>
+                    <th>status</th>
+                    <th>process</th>
+                </thead>
+                <tbody>
+
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 derece kullanılan monitör askısı</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="" class="btn btn-xs btn-danger btn-outline" title="delete"><i class="fa fa-trash"></i> Delete</a>
+                            <a href="" class="btn btn-xs btn-info btn-outline" title="edit"><i class="fa fa-pencil-square-o"></i> Edit</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 derece kullanılan monitör askısı</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="" class="btn btn-xs btn-danger btn-outline" title="delete"><i class="fa fa-trash"></i> Delete</a>
+                            <a href="" class="btn btn-xs btn-info btn-outline" title="edit"><i class="fa fa-pencil-square-o"></i> Edit</a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 derece kullanılan monitör askısı</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="" class="btn btn-xs btn-danger btn-outline" title="delete"><i class="fa fa-trash"></i> Delete</a>
+                            <a href="" class="btn btn-xs btn-info btn-outline" title="edit"><i class="fa fa-pencil-square-o"></i> Edit</a>
+                        </td>
+                    </tr>
+                </tbody>
+			</table>
+		</div><!-- .widget -->
+    </div>
+    <!-- END column -->
+</div>
