3940b3d69f80d107c8bf8f6b37515ddc2b62e4cb
Mehmet Kutuk <mkutuk42550@gmail.com>
2024-04-19 18:34:25 +0300

    sweetalert eklendi

---
+++ panel/assets/assets/js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function (){
+    swal({
+        title: "Good job!",
+        text: "You clicked the button!",
+        type: "success",
+        confirmButtonText: "ok"
+        })
+})
